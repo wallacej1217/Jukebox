34561f2c800a7713d73fcf3eb9e8c8812fd6ac3e
John Wallace <Wallacej2@student.lasalle.edu>
2017-10-17 17:12:50 -0400

    add
    added play, pause func.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
+var play = document.getElementsByClassName('play')[0]
+var pause = document.getElementsByClassName('pause')[0]
+var audio = document.getElementById('audio')
+console.log(audio)
 
+
+play.addEventListener('click', playAudio)
+pause.addEventListener('click', pauseAudio)
+
+function pauseAudio(){
+  audio.pause()
+}
+
+
+
+function playAudio(){
+  audio.play()
+  console.log(audio.duration)
+}
