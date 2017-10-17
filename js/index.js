f9b0d970e5858963dd6be7a373ed7ed2b0afb02c
John Wallace <Wallacej2@student.lasalle.edu>
2017-10-17 17:55:17 -0400

    worthless code added
    delete these  lines

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 var play = document.getElementsByClassName('play')[0]
 var pause = document.getElementsByClassName('pause')[0]
+var stop = document.getElementsByClassName('stop')[0]
 var audio = document.getElementById('audio')
 console.log(audio)
 
 
 play.addEventListener('click', playAudio)
 pause.addEventListener('click', pauseAudio)
-
+stop.addEventListener('click', pauseAudio)
 function pauseAudio(){
   audio.pause()
 }
@@ -16,4 +17,10 @@ function pauseAudio(){
 function playAudio(){
   audio.play()
   console.log(audio.duration)
+}
+
+Audio.prototype.stop = function()
+{
+    this.pause();
+    this.currentTime = 0.0;
 }
