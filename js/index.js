782656a0725494b8bdf4c89cff433d184b652abd
John Wallace <Wallacej2@student.lasalle.edu>
2017-10-18 16:34:11 -0400

    add
    addes next song, next button and background image.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var play = document.getElementsByClassName('play')[0]
 var pause = document.getElementsByClassName('pause')[0]
 var stop = document.getElementsByClassName('stop')[0]
+var next = document.getElementsByClassName('next')[0]
 var audio = document.getElementById('audio')
 console.log(audio)
 
@@ -9,6 +10,7 @@ console.log(audio)
 play.addEventListener('click', playAudio)
 pause.addEventListener('click', pauseAudio)
 stop.addEventListener('click', stopAudio)
+next.addEventListener('click', nextSong)
 function pauseAudio(){
   audio.pause()
 }
@@ -24,4 +26,9 @@ function playAudio(){
 {
     audio.pause();
     audio.currentTime = 0.0;
+}
+
+function nextSong(){
+  audio.src
+  audio.src = 'audio/ave.mp3'
 }
